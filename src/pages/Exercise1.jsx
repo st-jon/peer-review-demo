0cdd77fc07a5d5c9de29113334976650c82e220f
st-jon <c.johanny@gmail.com>
2026-04-15 20:13:52 +0200

    fix exercise 1

--- src/pages/Exercise1.jsx
+++ src/pages/Exercise1.jsx
@@ -33,7 +33,7 @@ export default function Exercise1() {
     >
       <div className="flex flex-col items-center gap-4 py-6">
         {/* TODO: add onClick={handleClick} to the button below */}
-        <button className="px-6 py-2 bg-indigo-600 text-white rounded-lg font-medium hover:bg-indigo-700 transition-colors">
+        <button onClick={() => handleClick()} className="px-6 py-2 bg-indigo-600 text-white rounded-lg font-medium hover:bg-indigo-700 transition-colors">
           Say Hello
         </button>
 
